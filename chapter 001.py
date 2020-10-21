8ffe745b9c59cb002e310afc5f4504e0d7cca326
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-21 09:04:44 +0300

    Update chapter 001.py
    more update on the list

--- chapter 001.py
+++ chapter 001.py
@@ -27,3 +27,70 @@ else:
     
 #check number of elements in a list
 print(len(myNewList))
+
+
+#append items on a list
+myNewList.append("Toshiba")
+print(myNewList)
+
+#insert at specific index
+myNewList.insert(1,"Lenovo")
+print(myNewList)
+
+#remove items using (pop)
+item_removed = myNewList.pop()
+print(item_removed)
+print(myNewList)
+
+#Remove a particular item
+myNewList.remove("Hp")
+print(myNewList)
+
+#remove all elements (clear)
+myNewList.clear()
+print(myNewList)
+
+#Reverse list
+myNewList = ["Hp", "Macbook","Lenovo","samsung"]
+myNewList.reverse()
+print(myNewList)
+
+#Sort List method
+myNewList.sort()
+print(myNewList)
+
+#new list with same items
+same_list = [5] * 5
+print(same_list)
+
+#List contatination
+list_1 = ["Hp", "Toshiba","Mac","carbon","Intel"]
+list_2 = [2.5, 3.5,10.6,12.5,15.5]
+new_list = list_1 + list_2 
+print(new_list)
+
+#list slicing, if no start specified it starts from begining, same for the end
+list_one = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+list_slice = list_one[2:]
+print(list_slice)
+#step index
+print(list_slice[::3])
+
+#copying a list
+#appending an item to the copy also modifies the copy and original list
+# to avoid modifiying the original list use (.copy method)
+original_list = ["owen",23,"Kabarak university"]
+copy_owen = original_list
+copy_owen.append("Chebara")
+print(copy_owen)
+print(original_list)
+
+#List comprehension
+original_elements = [2,4,6,8,10]
+square_elements = [i*i for i in original_elements]
+print(original_elements)
+print(square_elements)
+
+
+#UPNEXT_TUPPLES
+
