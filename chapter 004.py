d1801d9b34a060fd83d58329a83a91285b7a2423
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-31 02:42:28 +0300

    Update chapter 004.py
    decorators update

--- chapter 004.py
+++ chapter 004.py
@@ -1,1 +1,40 @@
 # DECORATORS
+
+# DECORATORS IN PYTHON
+
+# DECORATORS IN PYTHON
+# modifying classes and functions in python
+
+def before_after_dec(func):
+    def wrapper():
+        print('before this')
+        func()
+        print('after function')
+    return wrapper
+    
+def user_name():
+    print("Timz Owen")
+
+user_name = before_after_dec(user_name)
+
+user_name()
+
+# before this
+# Timz Owen
+# after function
+
+#same as  (using '@')
+def before_after_dec(func):
+    def wrapper():
+        print('before this')
+        func()
+        print('after function')
+    return wrapper
+@before_after_dec
+def user_name():
+    print("Timz Owen")
+user_name()
+
+
+# functions with arguments
+
