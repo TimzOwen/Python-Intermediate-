6095136707d2787a39812cea7c086a33dd0877e3
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-30 10:49:42 +0300

    Update chapter 003.py
    Exception handling update

--- chapter 003.py
+++ chapter 003.py
@@ -38,3 +38,102 @@ print(my_dict['year'])
 toshiba = 8
 if toshiba<16:
     raise Exception('You need additional Ram')
+    
+
+  #Raising exceptions
+
+#check computer's ram
+toshiba = 8
+if toshiba<16:
+    raise Exception('You need additional Ram')
+     
+# using Assert statement
+assert(toshiba>=16), 'More Ram needed to run android studio'
+
+# Try catch Exception block
+
+#division by 0 error
+try:
+    div = 5/0
+except:
+    print("No division by Zero allowed !")
+    
+# catch by exception 
+#division by 0 error
+try:
+    div = 5/0
+except Exception as e:
+    print(e)    # division by zero
+    
+
+# catch specific errors
+try:
+    div = 5/0
+    addString = 10 + '10'
+except ZeroDivisionError as e:
+    print(e)
+except TypeError as e:
+    print(e)
+    
+    
+# Finally and close Else
+try:
+    a = 10/0
+    b = a + 15
+except ZeroDivisionError as e:
+    print(e)
+except TypeError as e:
+    print(e)
+else:
+    print("all is OK")
+finally:
+    print("always running......if correct/not")
+    
+    
+
+# define error classes
+
+class NetWorkTooLow(Exception):
+    pass
+
+def checking_networkTraffic(i):
+    if i <500:
+        raise NetWorkTooLow('Low bandwidth in Data Link layer')
+    
+print(checking_networkTraffic(2000))
+
+#using try cath
+
+try:
+    checking_networkTraffic(250)
+except Exception as e:
+    print(e)
+
+# Try catch Exception block with defined func inside
+# takes in exception as the base class
+
+class TrafficTooHigh(Exception):
+    pass
+
+class TrafficToLow(Exception):
+    def __init__(self, warnings,exact_value):
+        self.warnings = warnings
+        self.exact_value = exact_value
+
+def check_traffic(x):
+    if x > 500:
+        raise TrafficTooHigh('Don\'t drive just board a cab')
+    if x<499:
+        raise TrafficTooHigh('You can drive safely',x)
+
+try:
+    check_traffic(10)
+except TrafficTooHigh as e:
+    print(e)
+except TrafficToLow as e:
+    print(e)
+    
+    
+    
+ 
+
