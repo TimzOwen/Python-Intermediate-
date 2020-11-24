d92d26083904518bf35551a1b79f3d30695d5ee9
Timz Owen <ktimothy@kabarak.ac.ke>
2020-11-24 04:22:47 +0300

    Update Python_challenges.py
    assignment task

--- Python_challenges.py
+++ Python_challenges.py
@@ -1,1 +1,17 @@
 
+def print_message(day):
+    messages = {
+        'monday': 'Let\'s Code Python',
+        'tuesday': 'We start writing scripts',
+        'wednesday': 'Time for Automation.',
+        'thursday': 'Find ad fix errors',
+        'friday': 'Read some novels',
+        'saturday': 'Go for meet ups',
+        'sunday': 'Go to church'
+    }
+    print(messages[day])
+
+def print_friday_message():
+    print_message('Friday')
+
+print_friday_message()
